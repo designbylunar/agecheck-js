64c11cd5830a553f34ff7828932b849cdddc4370
Matt Ronchetto <hello@doamatto.xyz>
2021-10-29 09:12:13 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ export default function agecheck(age: number, country: string): boolean {
         return false;
     } // Ensure country code is two characters long
 
-    if (!countries.hasOwnProperty(country)) {
+    if (!countries.hasOwnProperty(country) && age >= 13) {
         console.warn('[WARN] User\'s country is not in our database, but user is over 13. Proceed with caution as laws may apply that we are not aware of.');
         return true;
     }
